a8e3b47b0030412bfe60d0bcbdf7921124ce785e
NTheo <jeuxforum26@gmail.com>
2022-03-29 12:38:34 +0200

    DB models :+1:

---
+++ backend/app/data/models.py
@@ -0,0 +1,86 @@
+import uuid
+from datetime import datetime
+
+from pydantic import BaseModel
+from typing import List, Optional
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer, ForeignKey, UniqueConstraint, TIMESTAMP, JSON
+from sqlalchemy.orm import relationship
+from sqlalchemy.dialects.postgresql import UUID
+Base = declarative_base()
+
+
+class Users(Base):
+    """
+    User is generic gutenberg-API id container.
+    """
+    __tablename__ = "Users"
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    username = Column(String)
+    created_at = Column('created_at', TIMESTAMP(timezone=False),
+                        nullable=False, default=datetime.now())
+
+
+class Historic(Base):
+    """
+    """
+    __tablename__ = "Historic"
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    searchArgs = Column(JSON)
+    user_id = Column(UUID(as_uuid=True), ForeignKey("Users.id"))
+    created_at = Column('created_at', TIMESTAMP(timezone=False),
+                        nullable=False, default=datetime.now())
+
+    user = relationship("Users")
+
+
+class Books(Base):
+    __tablename__ = "Books"
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    title = Column(String)
+    full_text = Column(String)
+    author_id = Column(UUID(as_uuid=True), ForeignKey("Author.id"))
+    created_at = Column('created_at', TIMESTAMP(timezone=False),
+                        nullable=False, default=datetime.now())
+
+    author = relationship("Author")
+
+
+class Author(Base):
+    __tablename__ = "Author"
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    author_name = Column(String)
+    created_at = Column('created_at', TIMESTAMP(timezone=False),
+                        nullable=False, default=datetime.now())
+
+
+class SearchResult(Base):
+    __tablename__ = "SearchResult"
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    book_id = Column(UUID(as_uuid=True), ForeignKey("Books.id"))
+    historic_id = Column(UUID(as_uuid=True), ForeignKey("Historic.id"))
+    created_at = Column('created_at', TIMESTAMP(timezone=False),
+                        nullable=False, default=datetime.now())
+
+    book = relationship("Books")
+    historic = relationship("Historic")
+
+
+class Tags(Base):
+    __tablename__ = "Tags"
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    content = Column(String)
+    created_at = Column('created_at', TIMESTAMP(timezone=False),
+                        nullable=False, default=datetime.now())
+
+
+# MAP
+class Tagmaps(Base):
+    __tablename__ = "Tagmaps"
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    created_at = Column('timestamp', TIMESTAMP(timezone=False),
+                        nullable=False, default=datetime.now())
+    book_id = Column(UUID(as_uuid=True), ForeignKey("Books.id"))
+    tag_id = Column(UUID(as_uuid=True), ForeignKey("Tags.id"))
+    book = relationship("Books")
+    tag = relationship("Tags")
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from data.models import Base
 
 app = FastAPI()
 
+@app.on_event("startup")
+async def startup_event():
+    db_string = "postgresql://postgres:example@database:5432/gutenberg"
+    db_sal = create_engine(db_string)
+    Session = sessionmaker(db_sal)
+    session = Session()
+    Base.metadata.create_all(db_sal)
 
 @app.get("/")
 def helloworld():
