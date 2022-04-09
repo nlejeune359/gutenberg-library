3856742498e9cd3f166bc3d889e2e60190f263ab
NTheo <jeuxforum26@gmail.com>
2022-04-09 15:53:38 +0200

    multipleWordsSearch

--- backend/app/routers/simpleSearch.py
+++ backend/app/routers/simpleSearch.py
@@ -11,7 +11,8 @@ import uuid
 import inputArgsInJSON as aij
 
 router = APIRouter(
-            tags=["search", "simple"]
+            tags=["search", "simple"],
+            prefix="/simple"
             )
 
 
@@ -20,7 +21,7 @@ Session = sessionmaker(db_sal)
 session = Session()
 
 
-@router.get("/simple/{word}")
+@router.get("/oneword/{word}")
 async def searchOneWord(word: str, userId: str):
 
     try:
@@ -42,6 +43,39 @@ async def searchOneWord(word: str, userId: str):
         s_res = []
         for result in resFromDBTag:
             res.append(result.book.title)
+            s_res.append(SearchResult(book_id=result.book.id, historic_id=histoRow.id))
+
+        session.bulk_save_objects(s_res)
+        session.commit()
+
+        return res
+    else:
+        raise HTTPException(status_code=400, detail=userId+" : Doesnt exist")
+
+
+@router.get("/multiplewords/{words}")
+async def searchMultipleWords(words: str, userId: str):
+    try:
+        uuid.UUID(userId)
+    except ValueError:
+        raise HTTPException(status_code=400, detail=userId+" : Doesnt exist")
+
+    if session.query(Users).filter(Users.id == userId).count() > 0:
+        tokensSet, tokensList = createTokenList(words)
+
+        tagIDList = session.query(Tags.id).filter(Tags.content.in_(tokensList)).all()
+        resFromDBTag = session.query(Tagmaps).filter(Tagmaps.tag_id.in_(tagIDList)).all()
+
+        histoRow = Historic(searchArgs=aij.createJSON(tokensList), user_id=userId)
+        session.add(histoRow)
+        session.commit()
+        session.refresh(histoRow)
+
+        res = []
+        s_res = []
+        for result in resFromDBTag:
+            if result.book.title not in res:
+                res.append(result.book.title)
             s_res.append(SearchResult(book_id=result.book.id, historic_id=histoRow.id))
 
         session.bulk_save_objects(s_res)
